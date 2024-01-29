239a8a19612ff690a1d4562c99cdb6f71e618077
Jafarscript <lihammedjafar@gmail.com>
2024-01-29 23:51:20 +0100

    File Setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import SiderBar from "./components/SiderBar"
+import Movies from "./pages/Movies"
+import MoviesInfo from "./pages/MoviesInfo"
+
+const App = () => {
+  return (
+    <div className="flex">
+      <SiderBar />
+      <section className="p-5">
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MoviesInfo />} />
+        </Routes>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SiderBar.jsx
@@ -0,0 +1,23 @@
+import { RiMovie2Fill } from "react-icons/ri";
+import { BsSearch } from "react-icons/bs";
+import { FaHotjar } from "react-icons/fa";
+import { BiMovie } from "react-icons/bi";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiBookBookmark } from "react-icons/bi";
+import { RiArrowRightDoubleFill } from "react-icons/ri";
+import { FiSettings } from "react-icons/fi";
+// import {}
+
+
+const SiderBar = () => {
+  return (
+    <section className="p-5"> 
+        <h1 className="flex items-center gap-2"><RiMovie2Fill className="text-green-500 text-5xl"/> <span className="uppercase">MovieBase</span></h1>
+        {/* <nav>
+            Lin
+        </nav> */}
+    </section>
+  )
+}
+
+export default SiderBar
